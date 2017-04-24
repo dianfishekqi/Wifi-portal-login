a4c704fd9f86af8d24f8d42d0d5b8a9bb1d5ea5f
Dian Fishekqi <dianfishekqi@gmail.com>
2017-04-24 14:27:14 +0200

    Added install.sh,bug fix

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 Scripts that will automate login of wifi portals in Kosovo.
 I use cron to run the script every 20 mins.
 I've also made some mods to i3 config that so it hides the window of xterm.
-
+    Change wlan0 to your wireless interface(use iwconfig to find the interface)
 Dependencies:elinks,grep,curl
 
---
+++ install.sh
@@ -0,0 +1,21 @@
+if [[ $1 == "" ]]
+then
+  echo "Usage: ./install.sh wireless-interface
+        Please Lookup your interface by running iwconfig"
+else
+  
+  if [ "$EUID" -ne 0 ]
+  then 
+    echo "Please run as root"
+    exit
+  fi
+  
+  cDir=$(pwd)
+  cd $(dirname $0)
+  sed -i.orig "s/wlan0/$1/g" connectToVala
+  cp connectToVala /usr/bin/connectToVala
+  mv connectToVala.orig connectToVala
+  chmod +x /usr/bin/connectToVala
+  echo "You can run the script by writing: connectToVala"
+  cd $cdir
+fi
